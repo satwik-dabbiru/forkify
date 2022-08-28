fb310f539440af279c45ab14a844dc971bc61482
D.Satwik <satwikdabbiru@gmail.com>
2022-08-28 11:58:08 +0530

    Clean up

--- src/js/controller.js
+++ src/js/controller.js
@@ -101,9 +101,7 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-const newFeature = function () {
-  console.log('welcome to the application');
-};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipies);
@@ -114,8 +112,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
-  newFeature();
 };
 
 init();
